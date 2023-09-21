215b8e88cd8d7a4a8dc3520513cff569695f119a
Jesica <jesibjunco@hotmail.com>
2023-09-21 14:43:10 -0300

    buttonGBack

--- src/components/GoBackButton.jsx
+++ src/components/GoBackButton.jsx
@@ -10,7 +10,7 @@ const GoBackButton = () => {
     return (
         <button
             onClick={handleGoBack}
-            className="bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900 shadow-sm font-semibold text-base md:text-xl text-white rounded-full hover:shadow-2xl hover:bg-purple-900 border-2 border-opacity-50 border-white pt-1.5 pb-2 px-3 my-4"
+            className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  shadow-sm font-semibold text-base md:text-xl text-white hover:shadow-2xl hover:bg-purple-900 border-2 border-opacity-50 border-white pt-1.5 pb-2 px-3 my-4"
         >
             Go Back
         </button>
